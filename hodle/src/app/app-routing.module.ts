9df505408420d15f66baa575f4566248ff61e6f7
andbardii <andbardii@icloud.com>
2023-09-06 12:40:10 +0200

    services, module and route

--- hodle/src/app/app-routing.module.ts
+++ hodle/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) },
+  { path: 'wallet', loadChildren: () => import('./wallet/wallet.module').then(m => m.WalletModule) },
+  { path: 'market', loadChildren: () => import('./market/market.module').then(m => m.MarketModule) },
+  { path: 'todos', loadChildren: () => import('./todos/todos.module').then(m => m.TodosModule) },
+  { path: 'data', loadChildren: () => import('./data/data.module').then(m => m.DataModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
